cddb7e677933cfb67490cfa22265dd9c6bdc97e8
Alfonso Segura <a.segura.gonzalez@gmail.com>
2021-09-04 21:04:52 +0200

    added-restapi-models

---
+++ TryCatch.UnitTests/Models/CountResultTests.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="CountResultTests.cs" company="TryCatch Software Factory">
+// Copyright © TryCatch Software Factory All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+namespace TryCatch.UnitTests.Models
+{
+    using System;
+    using FluentAssertions;
+    using TryCatch.Models;
+    using Xunit;
+
+    public class CountResultTests
+    {
+        [Fact]
+        public void Construct_with_default_value()
+        {
+            // Arrange
+
+            // Act
+            var actual = new CountResult();
+
+            // Asserts
+            actual.Count.Should().Be(DefaultValues.DefaultCount);
+        }
+
+        [Fact]
+        public void Construct_with_custom_value()
+        {
+            // Arrange
+            var expected = 100;
+
+            // Act
+            var actual = new CountResult(expected);
+
+            // Asserts
+            actual.Count.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Construct_with_invalid_value()
+        {
+            // Arrange
+            var expected = -1;
+
+            // Act
+            Action actual = () => _ = new CountResult(expected);
+
+            // Asserts
+            actual.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
---
+++ TryCatch/Models/PageModel.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="PageModel.cs" company="TryCatch Software Factory">
+// Copyright © TryCatch Software Factory All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+namespace TryCatch.Models
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// Represents a standard model for paging queries.
+    /// </summary>
+    public class PageModel
+    {
+        private readonly string[] sortAsValidValues = new[]
+        {
+            DefaultValues.SortAsAscending,
+            DefaultValues.SortAsDescending,
+        };
+
+        private readonly bool sortAsAscendingByDefault;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageModel"/> class.
+        /// </summary>
+        public PageModel()
+        {
+            this.Offset = DefaultValues.DefaultOffset;
+            this.Limit = DefaultValues.DefaultPageLimit;
+            this.SortAs = DefaultValues.SortAsAscending;
+            this.sortAsAscendingByDefault = true;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageModel"/> class.
+        /// </summary>
+        /// <param name="limit">Sets the limit value - page size - for the query.</param>
+        /// <param name="offset">Sets the offset value, where the query must be started.</param>
+        /// <param name="orderBy">Sets the field to be used to sort the query results.</param>
+        /// <param name="sortAs">Sets the sort mode as ascending or descending. Only accepts ASC or DESC values.</param>
+        /// <param name="sortAsAscendingByDefault">Sets the default sort mode as ascending. It is used when the sortAs value is invalid or is not set.</param>
+        public PageModel(
+            int limit = DefaultValues.DefaultPageLimit,
+            int offset = DefaultValues.DefaultOffset,
+            string orderBy = "",
+            string sortAs = DefaultValues.SortAsAscending,
+            bool sortAsAscendingByDefault = true)
+        {
+            this.Limit = limit;
+            this.Offset = offset;
+            this.OrderBy = orderBy;
+            this.SortAs = sortAs;
+            this.sortAsAscendingByDefault = sortAsAscendingByDefault;
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the query.
+        /// </summary>
+        public int Limit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the offset to be applied in the query.
+        /// </summary>
+        public int Offset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the field name used as sort criteria.
+        /// </summary>
+        public string OrderBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort mode [ASC|DESC].
+        /// </summary>
+        public string SortAs { get; set; }
+
+        /// <summary>
+        /// Indicates if the query must be sorted as ascending.
+        /// </summary>
+        /// <returns>True value if it must be sorted as ascending.</returns>
+        public bool SortAsAscending()
+        {
+            var sortAsAscending = this.sortAsAscendingByDefault;
+
+            if (!string.IsNullOrWhiteSpace(this.SortAs) && this.sortAsValidValues.Contains(this.SortAs))
+            {
+                sortAsAscending = this.SortAs
+                    .ToUpper(CultureInfo.InvariantCulture)
+                    .Equals(DefaultValues.SortAsAscending, StringComparison.Ordinal);
+            }
+
+            return sortAsAscending;
+        }
+    }
+}
